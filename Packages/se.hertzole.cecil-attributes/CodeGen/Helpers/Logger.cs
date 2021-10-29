9e8066fa471670800d52618c3ece67128fbdb772
Hertzole <hampusringstrom@gmail.com>
2021-10-30 01:41:39 +0200

    Log a bit better

--- Packages/se.hertzole.cecil-attributes/CodeGen/Helpers/Logger.cs
+++ Packages/se.hertzole.cecil-attributes/CodeGen/Helpers/Logger.cs
@@ -6,11 +6,7 @@ namespace Hertzole.CecilAttributes.CodeGen
 	{
 		public static void Log(string message)
 		{
-#if UNITY_2020_2_OR_NEWER
 			Console.WriteLine(message);
-#else
-			UnityEngine.Debug.Log(message);
-#endif
 		}
 	}
 }
