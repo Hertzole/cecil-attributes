e440278d0cfee3bb48bffc762936c3ff6bc3c171
Hertzole <hampusringstrom@gmail.com>
2023-01-22 13:52:31 +0100

    fix: compile issues on versions before 2021.3

--- Packages/se.hertzole.cecil-attributes/Editor/Helpers/UiToolkitExtensions.cs
+++ Packages/se.hertzole.cecil-attributes/Editor/Helpers/UiToolkitExtensions.cs
@@ -4,11 +4,6 @@ namespace Hertzole.CecilAttributes.Editor
 {
 	internal static class UiToolkitExtensions
 	{
-		public static void MakeField<T>(this BaseField<T> element)
-		{
-			element.AddToClassList(BaseField<T>.alignedFieldUssClassName);
-		}
-
 		public static void RemoveDecorators(this VisualElement element)
 		{
 			VisualElement decorators = element.Q<VisualElement>(className: "unity-decorator-drawers-container");
